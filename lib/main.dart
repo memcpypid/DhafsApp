1a052f9bfa9f8c59df89cd79e8b80f089f92d076
memcpypid <darmaputra443@gmail.com>
2024-10-29 22:07:11 +0700

    Update Notification Handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dhafs_app/app/modules/controllers/auth_controllers.dart';
+import 'package:dhafs_app/app/modules/controllers/notification_handler.dart';
 import 'package:dhafs_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,8 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await Get.putAsync(() async => await SharedPreferences.getInstance());
+  await FirebaseMessagingHandler().initPushNotification();
+  await FirebaseMessagingHandler().initLocalNotification();
   runApp(
     GetMaterialApp(
       title: "Application",
